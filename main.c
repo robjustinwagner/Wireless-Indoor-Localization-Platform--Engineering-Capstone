52e773c13f441702f7996d57c044ea015e2f2063
coachcal43 <hareng@wisc.edu>
2014-12-06 11:32:48 -0600

    board working

--- main.c
+++ main.c
@@ -213,27 +213,6 @@ void General_Init()
 	DCOCTL = CALDCO_1MHZ;
 
 	for(j=2100;j>0;j--);             // Wait for DCO to stabilize.
-
-	//initialize all ports first ot minimize wasted current consumption
-	P1DIR = 0xFF;
-	P1OUT = 0x00;
-	P2DIR = 0xFF;
-	P2OUT = 0x00;
-	P3DIR = 0xFF;
-	P3OUT = 0x00;
-	P4DIR = 0xFF;
-	P4OUT = 0x00;
-	P5DIR = 0xFF;
-	P5OUT = 0x00;
-	P6DIR = 0xFF;
-	P6OUT = 0x00;
-	P7DIR = 0xFF;
-	P7OUT = 0x00;
-	P8DIR = 0xFF;
-	P8OUT = 0x00;
-	PADIR = 0xFF;
-	PAOUT = 0x00;
-
 }
 
 /*
